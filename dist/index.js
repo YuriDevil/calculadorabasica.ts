15a063c62f7f55b65c981b0a7eee3166c1b6ac19
YuriDevil <marcio.junior0107@gmail.com>
2023-10-19 17:59:09 -0300

    Fazer o Upload da calculadora TypeScript

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+const { add, subtract, multiply, divide } = require('./calculator');
+
+console.log('Addition:', add(5, 3));
+console.log('Subtraction:', subtract(5, 3));
+console.log('Multiplication:', multiply(5, 3));
+console.log('Division:', divide(6, 2));
